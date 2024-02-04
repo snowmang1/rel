7b32b2ae2671878b7195830bd8b578a48afba8f6
Evan Drake <evanmdrake@icloud.com>
2024-02-04 13:08:02 -0800

    version 1 complete, interp needs more testing but good enough for now
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib (
   interp,
-  findPat
+  findPat,
+  matchPre,
+  p2s
 ) where
 
 -- TODO:
@@ -15,15 +17,15 @@ import Structures
 interp :: RelIR -> String -> String
 interp [] fs = fs
 interp (x:ir) fs = case x of
-  Generate _ _ -> interp ir (genTerm x fs)
-  Replace _ _  -> interp ir (repTerm x fs)
+  Generate find gen -> interp ir (genTerm find gen fs)
+  Replace find rep  -> interp ir (repTerm find rep fs)
 interp l f = undefined
 
 -- | utility function for Union and Kleene mathcing
 eatPat :: Term -> String -> (String, Int)
 eatPat t' st' = aux t' st' 1 where
   aux :: Term -> String -> Int -> (String, Int)
-  aux t (h:st) i = if t==[h] then aux t st (i+1) else (st, i)
+  aux t (h:st) i = if t==[h] then aux t st (i+1) else (h:st, i)
   aux t [] i = ([], i)
 
 -- | Pattern to match -> file stream -> file stream after match
@@ -37,37 +39,82 @@ matchPat (patHead:pat) (fileHead:fs) =
   True -> matchPat pat fs
   False -> Nothing
 
+-- | match prefix will take a [Token] named a pattern and match a prefix 
+-- and return the length of the match or return Nothing
+matchPre :: [Token] -> String -> Maybe Int
+matchPre p fs = matchPre' p fs 0 where
+  matchPre' :: [Token] -> String -> Int -> Maybe Int
+  matchPre' [] [] end = if end/=0 then Just end else Nothing
+  matchPre' _ [] end  = Nothing
+  matchPre' p' (h:fs') end = 
+    case p' of
+    []  -> Just end
+    _   ->
+      case head p' of
+      Token Union s   -> case [h]==s of
+        True  -> let (fs'', end') = eatPat s fs' in matchPre' (tail p') fs'' (end+end')
+        False -> Nothing
+      Token Kleene s  -> case [h]==s of
+        True  -> let (fs'', end') = eatPat s fs' in matchPre' (tail p') fs'' (end+end')
+        False -> matchPre' (tail p') fs' (end+1)
+      Token Pattern s -> case matchPat s (h:fs') of
+        Just fs'' -> matchPre' (tail p') fs'' (end+(length s))
+        Nothing   -> Nothing
+      _ -> undefined
+
 -- | function to find index of some pattern [Token]
 findPat :: [Token] -> String -> Maybe (Int, Int)
 -- TODO: this function needs to fully reset [Token] upon failed match
-findPat p fs = findPat' p fs (0,0)
+findPat p fs = findPat' [] p fs (0,0)
   where
-  findPat' :: [Token] -> String -> (Int, Int) -> Maybe (Int, Int)
-  findPat' [] _ (start, end) = if start==end then Nothing else Just (start, end)
-  findPat' (Token Union s:pt) (h:fs') (start, end) =
+  findPat' :: [Token] -> [Token] -> String -> (Int, Int) -> Maybe (Int, Int)
+  findPat' _ _ [] (start, end) = if start==end then Nothing else Just (start, end)
+  findPat' mp (Token Union s:pt) (h:fs') (start, end) =
     if [h]==s then
       let (fs'',end') = eatPat s fs' in
-      findPat' pt fs'' (start, start+end')
+      findPat' (Token Union s:mp) pt fs'' (start, start+end')
     else
-      findPat' pt fs' (start+1,start+1)
-  findPat' (Token Kleene s:pt) (h:fs') (start, end) = 
+      findPat' [] ((reverse mp)++(Token Union s:pt)) fs' (start+1,start+1)
+  findPat' mp (Token Kleene s:pt) (h:fs') (start, end) = 
     if [h]==s then
       let (fs'',end') = eatPat s fs' in
-      findPat' pt fs'' (start, end+end')
+      findPat' (Token Kleene s:mp) pt fs'' (start, end+end')
     else
-      findPat' pt fs' (start, end+1)
-  findPat' (Token Pattern s:pt) fs' (start, end) =
+      case start==end of
+      True  -> findPat' (Token Kleene s:mp) pt fs' (start, end+1) --prefix non-disruptive
+      False -> findPat' [] ((reverse mp)++(Token Kleene s:pt)) fs' (start+1, start+1) -- disreuptive
+  findPat' mp (Token Pattern s:pt) fs' (start, end) =
     case matchPat s fs' of
-    Nothing -> findPat' pt fs' (start+1, start+1)
-    Just fs'' -> findPat' pt fs'' (start, end+(length s))
-  findPat' _ _ _ = undefined -- no Or for now, v1 /= Binary Ops
+    Nothing -> findPat' [] ((reverse mp)++(Token Pattern s:pt)) (tail fs') (start+1, start+1)
+    Just fs'' -> findPat' (Token Pattern s:mp) pt fs'' (start, end+(length s))
+  findPat' _ _ _ _ = undefined -- no Or for now, v1 /= Binary Ops
+
+-- | takes a list of tokens and creates an output string
+p2s :: [Token] -> String
+p2s p = p2s' p "" where
+  p2s' p a = case p of
+    []                 -> a
+    Token Union s:pt   -> p2s' pt (a ++ s)
+    Token Kleene s:pt  -> p2s' pt (a ++ "")
+    Token Pattern s:pt -> p2s' pt (a ++ s)
+    _ ->  p2s' (tail p) (a ++ "")
 
 -- | these will consume one OpStruct and preform the operation Generate on the given
 -- file string fs: find a pattern and append a string to it
-genTerm :: OpStruct -> String -> String
-genTerm op fs = fs
+genTerm :: [Token] -> [Token] -> String -> String
+genTerm find gen fs = genTerm' find gen fs "" where
+  genTerm' find' gen' fs' x =
+    case matchPre find' fs' of
+    Just end  -> let (pref, postf) = splitAt end fs' in
+      genTerm' find' gen' postf (x ++ pref ++ (p2s gen'))
+    Nothing   -> x
 
 -- | these will consume one OpStruct and preform the operation Replace on the given
 -- file string fs: find a pattern p and replace it with p'
-repTerm :: OpStruct -> String -> String
-repTerm op fs = fs
+repTerm :: [Token] -> [Token] -> String -> String
+repTerm find rep fs = repTerm' find rep fs "" where
+  repTerm' find' rep' fs' x =
+    case matchPre find' fs' of
+    Just end  -> let (pref, postf) = splitAt end fs' in
+      repTerm' find' rep' postf (x ++ (p2s rep'))
+    Nothing   -> x
--- test/Backend.hs
+++ test/Backend.hs
@@ -7,7 +7,7 @@ import Lib
 import Structures
 
 backend :: TestTree
-backend = testGroup "Backend Tests" [interpTests, findPatTests]
+backend = testGroup "Backend Tests" [interpTests, findPatTests, matchPreTests]
 
 interpTests :: TestTree
 interpTests = testGroup "interp Testing"
@@ -16,9 +16,15 @@ interpTests = testGroup "interp Testing"
     testCase "interp simple generate" $
       interp [Generate [Token Pattern "abc"] [Token Pattern "123"]] "abc"
       @?= "abc123",
-    testCase "interp simple generate" $
+    testCase "interp complex generate" $
+      interp [Generate [Token Union "h", Token Pattern "abc"] [Token Union "h", Token Kleene "i"]]
+        "hhhabc" @?= "hhhabch",
+    testCase "interp simple replace" $
       interp [Replace [Token Pattern "abc"] [Token Pattern "123"]] "abc"
-      @?= "123"
+      @?= "123",
+    testCase "interp complex replace" $
+      interp [Replace [Token Union "h", Token Pattern "abc"] [Token Pattern "123", Token Pattern "abc"]]
+      "habc" @?= "123abc"
   ]
 
 findPatTests :: TestTree
@@ -41,5 +47,39 @@ findPatTests = testGroup "findPat Testing"
     testCase "findPat single Pattern, single match" $
       findPat [Token Pattern "sap"] "sap" @?= Just (0,3),
     testCase "findPat two Pattern, two match" $
-      findPat [Token Pattern "hello", Token Pattern " World"] "hello World" @?= Just (0,11)
+      findPat [Token Pattern "hello", Token Pattern " World"] "hello World" @?= Just (0,11),
+    testCase "findPat Pattern after Failure, single match" $
+      findPat [Token Pattern "a"] "hello a" @?= Just (6,7),
+    testCase "findPat Union -> Pattern after Failure" $
+      findPat [Token Union "a", Token Pattern "hello"] "baaahello" @?= Just (1, 9),
+    testCase "findPat Kleene -> Pattern after Failure, actual match" $
+      findPat [Token Kleene "a", Token Pattern "hello"] "bahello" @?= Just (1, 7)
+  ]
+
+matchPreTests :: TestTree
+matchPreTests = testGroup "matchPre Testing"
+  [
+    testCase "matchPre single Union, single match" $
+      matchPre [Token Union "b"] "b" @?= Just 1,
+    testCase "matchPre single Union, no match" $
+      matchPre [Token Union "b"] "c" @?= Nothing,
+    testCase "matchPre single Union, multiple match" $
+      matchPre [Token Union "b"] "bbbb" @?= Just 4,
+    testCase "matchPre single Kleene, single match" $
+      matchPre [Token Kleene "b"] "b" @?= Just 1,
+    testCase "matchPre single Kleene, multiple match" $
+      matchPre [Token Kleene "b"] "bbbb" @?= Just 4,
+    testCase "matchPre single Kleene, no match" $
+      matchPre [Token Kleene "b"] "c" @?= Just 1,
+    testCase "matchPre single Pattern, no match" $
+      matchPre [Token Pattern "sap"] "sa" @?= Nothing,
+    testCase "matchPre single Pattern, single match" $
+      matchPre [Token Pattern "sap"] "sap" @?= Just 3,
+    testCase "matchPre two Pattern, two match" $
+      matchPre [Token Pattern "hello", Token Pattern " World"] "hello World" @?= Just 11,
+    testCase "matchPre Union -> Pattern -> Kleene" $
+      matchPre [Token Union "h", Token Pattern " hi ", Token Kleene "a"] "hhh hi aaaa" @?= Just 11,
+    testCase "matchPre Kleene -> Union -> Union -> Pattern" $
+      matchPre [Token Kleene "h", Token Union "i", Token Union "r", Token Pattern "hi"]
+        "hhhhhiiiiirrrrrrhi" @?= Just 18
   ]
